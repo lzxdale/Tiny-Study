26cc2b1be21fe4c5db5b7552e1036516648e4423
lzxdale <dalelin.zx@gmail.com>
2019-07-31 23:18:59 +0800

    Next page access

--- Scrapy/rental/rental/spiders/rental.py
+++ Scrapy/rental/rental/spiders/rental.py
@@ -5,6 +5,7 @@ from ..items import RentalItem
 class rentalSpider(scrapy.Spider):
     name = 'rental'
     start_urls = ['http://sz.ganji.com/zufang/pn1/']
+    host = '{}'
 
     def parse(self, response):
         print(response)
@@ -15,4 +16,10 @@ class rentalSpider(scrapy.Spider):
             rentalL['title'] = i
             rentalL['price'] = j
             yield rentalL
-
+        next_page = response.xpath('.//a[@class = "next"]/@href').extract()
+        if len(next_page) > 0:  # if next_page not exit, len will = 0 and not be excuted.
+            next_link = self.host.format(next_page[0])
+            print('***\n', next_link, '\n***', )
+            yield scrapy.Request(next_link, callback=self.parse)
+        else:
+            pass
