e641f60d97452388dc37cb133c6610965326a5f6
lzxdale <dalelin.zx@gmail.com>
2019-08-07 23:04:52 +0800

    user_agent_faker

---
+++ Scrapy/middleware_spbeen/middleware_spbeen/run_scrapy spbeen.py
@@ -0,0 +1,5 @@
+from scrapy.cmdline import execute
+
+## run this file, crawl will be run.
+
+execute('scrapy crawl spbeen'.split()) #muct be split
---
+++ Scrapy/middleware_spbeen/middleware_spbeen/spiders/spbeen.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+
+class SpbeenSpider(scrapy.Spider):
+    name = 'spbeen'
+    allowed_domains = ['http://www.spbeen.com/']
+    start_urls = ['http://www.spbeen.com/tool/request_info/']
+    start_url = 'http://www.spbeen.com/tool/request_info/'
+
+
+    def start_requests(self):  # make spider keep requesting for 10 times
+        for url in range(0,10):
+            yield scrapy.Request(self.start_url, dont_filter=True)
+
+    def parse(self, response):
+        info = {}
+        info['user_agent'] = response.xpath("normalize-space(.//div[@class = 'ui red segment']/div[@class = 'container']/text())").extract()
+        info['ip'] = response.xpath("normalize-space(.//div[@class = 'ui blue segment']/div[@class = 'container']/text())").extract()
+        print(info)
+
+
