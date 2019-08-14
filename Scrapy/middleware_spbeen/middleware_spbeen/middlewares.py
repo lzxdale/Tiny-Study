58921ad97d9e2760f1a667358daf151caeba36b2
lzxdale <dalelin.zx@gmail.com>
2019-08-13 22:42:47 -0400

    'Update'

--- Scrapy/middleware_spbeen/middleware_spbeen/middlewares.py
+++ Scrapy/middleware_spbeen/middleware_spbeen/middlewares.py
@@ -121,7 +121,7 @@ class MiddlewareSpbeenRandomUseragent2(UserAgentMiddleware):
 
     @classmethod
     def from_crawler(cls, crawler):
-        o = cls(Faker())
+        o = cls(Faker()) #this set the above user_agent as Faker()
         #o = cls(crawler.settings['USER_AGENT'])
         crawler.signals.connect(o.spider_opened, signal=signals.spider_opened)
         return o
