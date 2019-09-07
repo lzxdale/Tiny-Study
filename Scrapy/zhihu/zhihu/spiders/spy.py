5018e2862c290397a5fbbab50cc1b2a9d2d07255
lzxdale <dalelin.zx@gmail.com>
2019-09-06 23:53:51 -0400

    'loading_more'

--- Scrapy/zhihu/zhihu/spiders/spy.py
+++ Scrapy/zhihu/zhihu/spiders/spy.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import json
+from lxml import etree
 
 class SpySpider(scrapy.Spider):
     name = 'spy'
@@ -10,5 +11,28 @@ class SpySpider(scrapy.Spider):
     def parse(self, response):
         topics = response.xpath('.//body//ul[@class="zm-topic-cat-main clearfix"]/li')
         for li in topics:
-            print(li.xpath('./a/text()').extract_first(),':',li.xpath('./@data-id').extract_first())
-        print(len(topics))
+            name = li.xpath('./a/text()').extract_first()
+            topic = li.xpath('./@data-id').extract_first()
+            print(name, topic)
+            yield scrapy.FormRequest('https://www.zhihu.com/node/TopicsPlazzaListV2', callback=self.parse_topic, dont_filter=True, meta={"offset":20,"topic_id":topic, "name":name},
+                                 formdata={"method": "next", "params": json.dumps({"topic_id":topic,"offset":20,"hash_id":""})})
+
+    def parse_topic(self,response):
+        offset = response.meta.get('offset')
+        topic_id = response.meta.get('topic_id')
+        msg_info = json.loads(response.text)
+        name = response.meta.get("name")
+        # print("offset: ", offset)
+        offset += len(msg_info['msg'])
+        for m in msg_info['msg']:
+            html = etree.HTML(m)
+            href = html.xpath(".//a[@target='_blank']/@href")
+            tag = html.xpath(".//strong/text()")
+            p_content = html.xpath(".//p/text()")
+            #print(href, name, p_content)
+        if not len(msg_info['msg']) < 20:
+            yield scrapy.FormRequest('https://www.zhihu.com/node/TopicsPlazzaListV2', callback=self.parse_topic,
+                                    dont_filter=True, meta={"offset": offset, "topic_id":topic_id, "name":name},
+                                    formdata={"method": "next", "params": json.dumps({"topic_id": topic_id, "offset": offset, "hash_id": ""})})
+        else:
+            print("name:{},offset:{}".format(name,offset))
